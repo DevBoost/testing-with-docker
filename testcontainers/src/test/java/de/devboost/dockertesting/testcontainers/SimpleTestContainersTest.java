ec6952f5a7eec7c1e31a0a1cd67ebd0ad6aa93f3
Mirko Seifert <mirko.seifert@kiwigrid.com>
2018-04-02 13:15:13 +0200

    - Added initial example for creating a populated MySQL image

--- testcontainers/src/test/java/de/devboost/dockertesting/testcontainers/SimpleTestContainersTest.java
+++ testcontainers/src/test/java/de/devboost/dockertesting/testcontainers/SimpleTestContainersTest.java
@@ -1,5 +1,7 @@
 package de.devboost.dockertesting.testcontainers;
 
+import java.sql.*;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -13,10 +15,24 @@ public class SimpleTestContainersTest {
 	@Rule
 	public GenericContainer mysqlContainer = new GenericContainer("mysql:5.7.21")
 			.withEnv("MYSQL_ROOT_PASSWORD", "secret")
+			.withEnv("MYSQL_DATABASE", "testdb")
 			.withExposedPorts(3306);
 
 	@Test
 	public void canStartAndStopMySqlContainer() {
 		LOGGER.info("Container ID is {}", mysqlContainer.getContainerId());
+	}
+
+	@Test
+	public void canListDatabaseTables() throws SQLException {
+		int mappedPort = mysqlContainer.getMappedPort(3306);
+		String connectionUrl = "jdbc:mysql://localhost:" + mappedPort + "/testdb";
+		try (Connection connection = DriverManager.getConnection(connectionUrl, "root", "secret")) {
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery("SHOW TABLES;");
+			while (resultSet.next()) {
+				LOGGER.info("Found table '{}'", resultSet.getString(1));
+			}
+		}
 	}
 }
